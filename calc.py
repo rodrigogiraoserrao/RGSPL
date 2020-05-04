43e7ab2363d98f928f35301df80cb21f09b29b86
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-05-04 23:59:07 +0100

    Fix typo.

--- calc.py
+++ calc.py
@@ -229,7 +229,7 @@ class Parser:
 def main():
     while (inp := input(" >> ")):
         lexer = Lexer(inp)
-        parse = Parser(lexer)
+        parser = Parser(lexer)
         result = parser.parse()
         print(result)
 
