56ba10564fc2f88ad31cae8c1f4db9aa198e161a
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-08-15 22:30:51 +0100

    Implement Over.

--- doperators.py
+++ doperators.py
@@ -16,3 +16,21 @@ def jot(*, aalpha, oomega):
     def derived(*, alpha=None, omega):
         return aalpha(alpha=alpha, omega=oomega(omega=omega))
     return derived
+
+def over(*, aalpha, oomega):
+    """Define the dyadic over ⍥ operator.
+
+    Monadic case:
+        f⍥g ⍵
+    f g ⍵
+    Dyadic case:
+        ⍺ f⍥g ⍵
+    (g ⍺) f (g ⍵)
+    """
+
+    def derived(*, alpha=None, omega):
+        if alpha is None:
+            return aalpha(alpha=alpha, omega=oomega(omega=omega))
+        else:
+            return aalpha(alpha=oomega(omega=alpha), omega=oomega(omega=omega))
+    return derived
