6b617147c7fa7c0247728bec65e197733c582a63
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-10-22 01:10:53 +0100

    Start testing Tokenizer.

--- testing.py
+++ testing.py
@@ -2,13 +2,91 @@
 Tests the current subset of APL implemented.
 """
 
-from rgspl import Interpreter, Parser, Tokenizer
+import unittest
+
+from rgspl import Interpreter, Parser, Tokenizer, Token
 from arraymodel import APLArray
 
 run = lambda code: Interpreter(Parser(Tokenizer(code))).interpret()
 
 def S(scalar):
     return APLArray([], scalar)
+
+class TestTokenizer(unittest.TestCase):
+    """Test the tokenizer."""
+
+    tok = lambda _, s: Tokenizer(s).tokenize()
+    eof = Token(Token.EOF, None)
+
+    def test_integers(self):
+        """Test integer tokenization."""
+
+        for n in range(30):
+            self.assertEqual(self.tok(str(n)), [self.eof, Token(Token.INTEGER, n)])
+            self.assertEqual(self.tok(f"¯{n}"), [self.eof, Token(Token.INTEGER, -n)])
+
+    def test_floats(self):
+        """Test float tokenization."""
+
+        self.assertEqual(self.tok("0.5"), [self.eof, Token(Token.FLOAT, 0.5)])
+        self.assertEqual(self.tok("¯0.25"), [self.eof, Token(Token.FLOAT, -0.25)])
+        self.assertEqual(self.tok(".125"), [self.eof, Token(Token.FLOAT, 0.125)])
+        self.assertEqual(self.tok("¯.0625"), [self.eof, Token(Token.FLOAT, -0.0625)])
+
+    def test_complex_nums(self):
+        """Test complex number tokenization."""
+
+        T = Token
+        C = lambda c: [self.eof, T(T.COMPLEX, c)]
+        realss = ["0", "2", "¯3", "0.5", "¯0.25", ".125", "¯.5"]
+        realsv = [0, 2, -3, 0.5, -0.25, 0.125, -0.5]
+        compss = ["1", "¯4", "0.1", "¯0.01", ".2", "¯.8"]
+        compsv = [1, -4, 0.1, -0.01, 0.2, -0.8]
+        for rs, rv in zip(realss, realsv):
+            for cs, cv in zip(compss, compsv):
+                c = complex(rv, cv)
+                with self.subTest(c=c):
+                    self.assertEqual(self.tok(f"{rs}J{cs}"), C(c))
+
+    def test_numeric_upgrades(self):
+        """Ensure empty imaginary parts and empty decimals get promoted."""
+
+        f = lambda t: lambda v: [self.eof, Token(t, v)]
+        I = f(Token.INTEGER)
+        F = f(Token.FLOAT)
+
+        self.assertEqual(self.tok("1."), I(1))
+        self.assertEqual(self.tok("56.0"), I(56))
+        self.assertEqual(self.tok("¯987."), I(-987))
+        self.assertEqual(self.tok("¯23.0"), I(-23))
+
+        self.assertEqual(self.tok("1J0"), I(1))
+        self.assertEqual(self.tok("2J¯0"), I(2))
+        self.assertEqual(self.tok("1.2J0"), F(1.2))
+        self.assertEqual(self.tok("¯0.5J0"), F(-0.5))
+
+        self.assertEqual(self.tok("1.0J0."), I(1))
+        self.assertEqual(self.tok("¯8.J0.0"), I(-8))
+        self.assertEqual(self.tok(".125J0.0"), F(0.125))
+        self.assertEqual(self.tok("¯.25J0."), F(-0.25))
+
+class TestScalarParsing(unittest.TestCase):
+    """Test that scalars are parsed conveniently."""
+
+    def test_nonneg_integers(self):
+        self.assertEqual(run("0"), S(0))
+        self.assertEqual(run("1"), S(1))
+        self.assertEqual(run("2"), S(2))
+        self.assertEqual(run("123456789987654321"), S(123456789987654321))
+
+    def test_negative_integers(self):
+        self.assertEqual(run("¯1"), S(-1))
+        self.assertEqual(run("¯2"), S(-2))
+        self.assertEqual(run("¯0"), S(0))
+        self.assertEqual(run("¯973"), S(-973))
+
+if __name__ == "__main__":
+    unittest.main()
 
 def raw_data_test(code, raw):
     """Test if the given code gives the expected raw result."""
