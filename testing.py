f08a85d1e89bf85b78350602358957367b796256
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2021-01-16 15:27:54 +0000

    Test scalar and array evaluation.

--- testing.py
+++ testing.py
@@ -110,8 +110,8 @@ class TestTokenizer(unittest.TestCase):
                 with self.subTest(code=code):
                     self.assertEqual(self.tok(code), toks)
 
-class TestScalarParsing(unittest.TestCase):
-    """Test that scalars are parsed conveniently."""
+class TestScalarEvaluation(unittest.TestCase):
+    """Test that scalars are evaluated conveniently."""
 
     def test_nonneg_integers(self):
         self.assertEqual(run("0"), S(0))
@@ -125,56 +125,56 @@ class TestScalarParsing(unittest.TestCase):
         self.assertEqual(run("¯0"), S(0))
         self.assertEqual(run("¯973"), S(-973))
 
+    def test_floats(self):
+        self.assertEqual(run("0.0"), S(0))
+        self.assertEqual(run("0.5"), S(0.5))
+        self.assertEqual(run("¯0.25"), S(-0.25))
+        self.assertEqual(run("¯0.125"), S(-0.125))
+
+    def test_complex_numbers(self):
+        self.assertEqual(run("0J0"), S(0))
+        self.assertEqual(run("1J1"), S(1 + 1j))
+        self.assertEqual(run("3J¯0.5"), S(3 - 0.5j))
+        self.assertEqual(run("¯0.3J15"), S(-0.3 + 15j))
+
+    def test_redundant_parens(self):
+        self.assertEqual(run("(0.2)"), S(0.2))
+        self.assertEqual(run("(((¯3)))"), S(-3))
+        self.assertEqual(run("((((((((3J4))))))))"), S(3 + 4j))
+
+class TestArrayEvaluation(unittest.TestCase):
+    """Test that arrays (that are not scalars) are evaluated conveniently."""
+
+    def test_simple_arrays(self):
+        self.assertEqual(run("1 2 3 4"), APLArray([4], [S(1), S(2), S(3), S(4)]))
+        self.assertEqual(run("¯1 1 ¯1 1"), APLArray([4], [S(-1), S(1), S(-1), S(1)]))
+        self.assertEqual(run("(2.3 5.6 7.8 9)"), APLArray([4], [S(2.3), S(5.6), S(7.8), S(9)]))
+        self.assertEqual(run("0J1 ¯1J0 0J¯1 1"), APLArray([4], [S(1j), S(-1), S(-1j), S(1)]))
+
+    def test_nested_vectors(self):
+        self.assertEqual(
+            run("(0 1) (2 3) (4 5)"),
+            APLArray([3], [APLArray([2], [S(0), S(1)]), APLArray([2], [S(2), S(3)]), APLArray([2], [S(4), S(5)])])
+        )
+        self.assertEqual(
+            run("1 2 3 (4 5) (6 7)"),
+            APLArray([5], [S(1), S(2), S(3), APLArray([2], [S(4), S(5)]), APLArray([2], [S(6), S(7)])])
+        )
+        self.assertEqual(
+            run("1 (2 (3 (4 5)))"),
+            APLArray([2], [S(1), APLArray([2], [S(2), APLArray([2], [S(3), APLArray([2], [S(4), S(5)])])])])
+        )
+        self.assertEqual(
+            run("(((1 2) 3) 4) 5"),
+            APLArray([2], [APLArray([2], [APLArray([2], [APLArray([2], [S(1), S(2)]), S(3)]), S(4)]), S(5)])
+        )
+
 if __name__ == "__main__":
     unittest.main()
 
-def raw_data_test(code, raw):
-    """Test if the given code gives the expected raw result."""
-    assert run(code) == raw
-
-def test(code, expected):
-    """Test if the given code is interpreted to the expected result."""
-    assert run(code) == run(expected)
-
-# Test simple scalars on their own.
-raw_data_test("5", S(5))
-raw_data_test("¯2", S(-2))
-raw_data_test("123456", S(123456))
-raw_data_test("5.6", S(5.6))
-raw_data_test("1.0", S(1))
-raw_data_test("¯05.06", S(-5.06))
-raw_data_test("¯000.001", S(-0.001))
-raw_data_test("0J1", S(complex(0, 1)))
-raw_data_test("56J0.002", S(complex(56, 0.002)))
-raw_data_test("102.5J1", S(complex(102.5, 1)))
-raw_data_test("1J0", S(1))
-raw_data_test("¯3.7J0.0", S(-3.7))
-
-# Test simple vectors on their own.
-raw_data_test("1 2 3 4", APLArray([4], [S(1), S(2), S(3), S(4)]))
-raw_data_test("¯1 1 ¯1 1", APLArray([4], [S(-1), S(1), S(-1), S(1)]))
-raw_data_test("(2.3 5.6 7.8 9)", APLArray([4], [S(2.3), S(5.6), S(7.8), S(9)]))
-
-# Test redundant parenthesis
-raw_data_test("(0.2)", S(0.2))
-raw_data_test("((5))", S(5))
-raw_data_test("(((¯3)))", S(-3))
-
-# Test nested vectors
-raw_data_test(
-    "1 2 3 (4 5) (6 7)",
-    APLArray([5], [S(1), S(2), S(3), APLArray([2], [S(4), S(5)]), APLArray([2], [S(6), S(7)])])
-)
-raw_data_test(
-    "1 (2 (3 (4 5)))",
-    APLArray([2], [S(1), APLArray([2], [S(2), APLArray([2], [S(3), APLArray([2], [S(4), S(5)])])])])
-)
-raw_data_test(
-    "(((1 2) 3) 4) 5",
-    APLArray([2], [APLArray([2], [APLArray([2], [APLArray([2], [S(1), S(2)]), S(3)]), S(4)]), S(5)])
-)
-
+"""
 # Test nesting of scalars
 test("⊂1", "1")
 test("⊂⊂1", "1")
 test("⊂⊂⊂¯3.5", "¯3.5")
+"""
